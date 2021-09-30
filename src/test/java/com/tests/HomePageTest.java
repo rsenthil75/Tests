75df004ea0537f8755535b79d942deb73794ea45
Senthil <rsenthil75@hotmail.com>
2021-09-29 20:23:10 -0500

    Updated.

--- src/test/java/com/tests/HomePageTest.java
+++ src/test/java/com/tests/HomePageTest.java
@@ -2,18 +2,33 @@ package com.tests;
 
 
 import com.sample.practice.Pages;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HomePageTest {
     @Test
-    public void checkHomePage() {
+    public void testCheckHomePage() {
         String url="https://www.selenium.dev";
         String title ="Selenium";
-        Pages.homePage().goTo(url);
+        Pages.homePage().open(url);
         assertEquals(Pages.homePage().getTitle(),title);
-        Pages.homePage().close();
     }
+
+    @Test
+    public void testCheckDownloadPage() {
+        String url="https://www.selenium.dev/downloads/";
+        String title ="Downloads | Selenium";
+        Pages.downloadPage().open(url);
+        assertEquals(Pages.downloadPage().getTitle(),title);
+    }
+
+    @AfterAll
+    public void testClose(){
+        Pages.close();
+    }
+
 
 }
